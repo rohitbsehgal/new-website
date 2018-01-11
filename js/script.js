c1b8a84e041615f00829f7bfbe6fa5b7a85cb508
rohit sehgal <rohitsehgal1998@gmail.com>
2018-01-11 14:49:32 -0600

    responsive
    this literally took forever, its now responsive babayyy

--- js/script.js
+++ js/script.js
@@ -5,20 +5,44 @@ var distance1 = $('.stickyBanner1').offset().top, $window = $(window);
 var divNum = 0;
 var i = 0;
 var x = 0;
-$window.scroll(function() {
-height = $(divs[i]).height();
-var top = $window.scrollTop() ;
-var windowHeight = $(window).height();
-console.log(top);
-console.log(windowHeight*(i+1));
-console.log(i);
-if( top - windowHeight*(i+1) >= 0  && i < divs.length-1){
-  $(divs[i]).css({position: "fixed", top: "0px"});
-  i++
-}else if(i >= 1 && top - windowHeight/(i+1) >= 0){
-  $(divs[i]).css({position: "relative", top: (i+1) + "00%"});
-  i--;
-}else{
-  $(divs[i]).css({position: "relative", top: (i+1) + "00%"});
+var height2 = height;
+var bottoms = [];
+if($window.width() <= 575){
+for(var j = 0; j< divs.length; j++){
+  var bottom = $(divs[j]).height();
+  bottoms.push(bottom);
 }
+}
+$window.scroll(function() {
+  if($window.width() > 575){
+    height = $(divs[i]).height();
+    var top = $window.scrollTop() ;
+    var windowHeight = $(window).height();
+    if( top - windowHeight*(i+1) >= 0  && i < divs.length-1){
+      $(divs[i]).css({position: "fixed", top: "0px"});
+      i++
+    }else if(i >= 1 && top - windowHeight/(i+1) >= 0){
+      $(divs[i]).css({position: "relative", top: (i+1) + "00%"});
+      i--;
+    }else{
+      $(divs[i]).css({position: "relative", top: (i+1) + "00%"});
+    }
+  }else{
+    height = $(divs[i]).height();
+    var top = $window.scrollTop();
+    var windowHeight = $(window).height();
+    if(top >= height  && i < divs.length-1){
+      $(divs[i]).css({position: "fixed", bottom: "0px", top: "auto"});
+      top = 0;
+      i++;
+      window.scrollTo(0, 0);
+    }else if(i >= 1 && top <= 0){
+      i--;
+      $(divs[i]).css({position: "relative", top: "100%", bottom: "auto"});
+      window.scrollTo(0, bottoms[i]);
+
+    }else if(i < 1){
+      $(divs[i]).css({position: "relative", top: "100%"});
+    }
+  }
 });
