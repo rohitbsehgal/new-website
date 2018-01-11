ed96144efc07af3c9a4fce4104ab27d9d5b82ec0
rohit sehgal <rohitsehgal1998@gmail.com>
2018-01-11 17:46:30 -0600

    a

--- js/script.js
+++ js/script.js
@@ -41,28 +41,6 @@ $window.scroll(function() {
       $(divs[i]).css({position: "relative", top: "100%", bottom: "auto"});
       window.scrollTo(0, bottoms[i]);
 
-    }else if(i < 1){
-      $(divs[i]).css({position: "relative", top: "100%"});
     }
   }
 });
-var attachMobileSafariAddressBarHelpTip = function (target) {
-    var $target = $(target);
-    $target.tooltip({
-        title: 'Scroll up to hide Safari address bar',
-        trigger: 'manual',
-        placement: 'bottom'
-    });
-    $(window).on('resize', function () {
-        var bodyHeight = document.body.offsetHeight;
-        var windowHeight = window.innerHeight;
-        var isLandscape = Math.abs(window.orientation) === 90;
-        var showTooltip = (windowHeight < bodyHeight);
-        if(!isLandscape) return;
-        $target.tooltip(showTooltip ? 'show' : 'hide');
-    });
-}
-var ua = window.navigator.userAgent;
-if(ua.indexOf('iPhone') !== -1 && ua.indexOf('Safari') !== -1) {
-    attachMobileSafariAddressBarHelpTip('#main-nav');
-}
