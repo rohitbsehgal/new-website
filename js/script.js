89d24c979fdf3dfa7f0bb156407ed2270a9b8445
rohit sehgal <rohitsehgal1998@gmail.com>
2018-01-12 14:06:46 -0600

    responsive
    safari is such a hassle lol

--- js/script.js
+++ js/script.js
@@ -29,18 +29,25 @@ $window.scroll(function() {
     }
   }else{
     height = $(divs[i]).height();
-    var top = $window.scrollTop();
+    var top = $("body").scrollTop();
+    console.log(top);
+    console.log(height);
+    console.log(i);
     var windowHeight = $(window).height();
     if(top >= height  && i < divs.length-1){
       $(divs[i]).css({position: "fixed", bottom: "0px", top: "auto"});
+      console.log("first");
       top = 0;
       i++;
-      window.scrollTo(0, 0);
-    }else if(i >= 1 && top <= 0){
-      i--;
+      window.scrollTo(0, 10);
+    }else if(i >= 1 && top <= 0 ){
+      console.log("second");
       $(divs[i]).css({position: "relative", top: "100%", bottom: "auto"});
-      window.scrollTo(0, bottoms[i]);
+      i--;
+      window.scrollTo(0, bottoms[i] - 10);
 
+    }else{
+      $(divs[i]).css({position: "relative", top: "100%", bottom: "auto"});
     }
   }
 });
